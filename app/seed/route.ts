1264ef3da95ee5efd056114af80f7e6fddec34ec
Nadine Castro <nadine.castro@loblaw.ca>
2025-03-20 15:02:28 -0400

    updated query and seed script to use neo

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,8 +1,8 @@
 import bcryptjs from "bcryptjs";
-import postgres from "postgres";
+import { neon } from "@neondatabase/serverless";
 import { invoices, customers, revenue, users } from "../lib/placeholder-data";
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+const sql = neon(process.env.DATABASE_URL!);
 
 async function seedUsers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
@@ -103,15 +103,16 @@ async function seedRevenue() {
 
 export async function GET() {
   try {
-    const result = await sql.begin((sql) => [
-      seedUsers(),
-      seedCustomers(),
-      seedInvoices(),
-      seedRevenue(),
-    ]);
-
+    await sql`BEGIN`; // Start transaction
+    await seedUsers();
+    await seedCustomers();
+    await seedInvoices();
+    await seedRevenue();
+    await sql`COMMIT`; // Commit transaction
     return Response.json({ message: "Database seeded successfully" });
   } catch (error) {
+    await sql`ROLLBACK`; // Rollback transaction on error
+    console.error("Seeding error:", error);
     return Response.json({ error }, { status: 500 });
   }
 }
